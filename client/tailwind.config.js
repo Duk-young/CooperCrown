cc5ee5c5df40e971a4871914fad63a1913622607
Sang Min Shin <smshin0703@gmail.com>
2023-07-06 00:47:30 -0400

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -355,6 +355,14 @@ module.exports = {
 				A200: '#18FFFF',
 				A400: '#00E5FF',
 				A700: '#00B8D4'
+			},
+			'primary-green': {
+				default: '#368C69',
+				100: '#368C69',
+			},
+			'secondary-green': {
+				default: '#D1E8DD',
+				100: '#D1E8DD',
 			}
 		},
 		spacing: {
